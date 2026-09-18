3138632e81877c6f21d1a4ba6c4e7e6379d7d978
jan2xo <163499503+jan2xo@users.noreply.github.com>
2026-09-18 11:51:36 -0700

    refactor(dotnet): return native License Center exit codes

--- dotnet/src/BKE.LicensingAgent.Desktop/Program.cs
+++ dotnet/src/BKE.LicensingAgent.Desktop/Program.cs
@@ -5,7 +5,8 @@ namespace BKE.LicensingAgent.Desktop;
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static int Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     public static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>()
